4cc247804f7f7024a12ff1716cb4261d7f376e9a
Sinisti! <Spideralan2008@gmail.com>
2024-11-14 16:23:42 +0100

    Final Touches

--- app/src/main/java/com/example/domekwgorach/MainActivity.java
+++ app/src/main/java/com/example/domekwgorach/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.domekwgorach;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
     private TextView likes;
     private Button like;
@@ -19,17 +13,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         like = findViewById(R.id.like);
         save = findViewById(R.id.save);
         delete = findViewById(R.id.delete);
         likes = findViewById(R.id.likes);
-        EdgeToEdge.enable(this);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         like.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    void liked(){
-       likesi++;
-       likes.setText(likesi + " polubień.");
+    void liked() {
+        likesi++;
+        likes.setText(likesi + " polubień.");
     }
-    void deleted(){
-        if(likesi!=0) {
+    void deleted() {
+        if (likesi != 0) {
             likesi--;
             likes.setText(likesi + " polubień.");
         }
